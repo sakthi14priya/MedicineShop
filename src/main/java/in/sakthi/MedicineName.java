31433ab7ebb65cff107b88f4b7f73087930d2aaf
sakthi14priya <82450038+sakthi14priya@users.noreply.github.com>
2021-05-10 21:26:18 +0530

    Add files via upload

---
+++ src/main/java/in/sakthi/MedicineName.java
@@ -0,0 +1,11 @@
+package in.sakthi;
+
+public class MedicineName {
+	public static String name(String[] medicineName) {
+		for (String i : medicineName) {
+			System.out.println(i);
+		}
+		return null;
+
+	}
+}
