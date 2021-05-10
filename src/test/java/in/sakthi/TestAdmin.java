ee64fa0d1e45c3e914a9d87d30e992c75464b0c2
sakthi14priya <82450038+sakthi14priya@users.noreply.github.com>
2021-05-10 21:22:21 +0530

    Add files via upload

---
+++ src/test/java/in/sakthi/TestAdmin.java
@@ -0,0 +1,14 @@
+package in.sakthi;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestAdmin {
+
+	@Test
+	public void test() {
+		AddDetails.medicineName();
+	}
+
+}
